028248de0fee86da59450a476dc0a1b69136d09a
fatih <fatihaydilek@gmail.com>
2019-05-26 21:15:20 +0300

    add get_item and update_item functions to dynamoDb utility

--- dynamo_utility.py
+++ dynamo_utility.py
@@ -1,8 +1,8 @@
 import logging
 import boto3
+import json
 
 dynamodb = boto3.resource('dynamodb')
-
 
 def insert_item(table_name, item):
     try:
@@ -12,3 +12,41 @@ def insert_item(table_name, item):
         logging.error("Failed inserting item. Continuing. {}".format(e))
     else:
         logging.info("insert item successful")
+
+
+def get_item(table_name, id):
+    try:
+        table = dynamodb.Table(table_name)
+
+        result = table.get_item(
+            Key={
+                'id': id
+            }
+        )
+    except Exception as e:
+        logging.error("Failed getting item. Continuing. {}".format(e))
+        return None
+    else:
+        if 'Item' in result:
+            return result['Item']
+
+
+def update_item(table_name, item):
+    table = dynamodb.Table(table_name)
+
+    result = table.update_item(
+        Key={
+            'id': item['id']
+        },
+        UpdateExpression="set #st = :s, s3Url =:s3",
+        ExpressionAttributeValues={
+            ':s': item['state'],
+            ':s3': item['s3Url'],
+        },
+        ReturnValues='UPDATED_NEW',
+        ExpressionAttributeNames={
+            "#st": "state"
+        }
+    )
+
+    return result['Attributes']
